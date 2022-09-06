8645134636dc583991e28d8706a7797cd714dc52
User <nicholas.v.ndiki@gmail>
2022-09-07 01:56:35 +0300

    Fix - backend codebase

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = '';
+const apiId = 'qj0c402vw1';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-3v0c4-j1.us.auth0.com',
+  clientId: 'QGAT8lZnZPwUsGS29KwSNGt297IOou65',
   callbackUrl: 'http://localhost:3000/callback'
 };
