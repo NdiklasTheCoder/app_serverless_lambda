69ca3e7b976eafed0a19c642b459fa41bf00d802
User <nicholas.v.ndiki@gmail>
2022-09-08 04:16:11 +0300

    Refactor - updated utils function logic v1

--- backend/src/lambda/utils.ts
+++ backend/src/lambda/utils.ts
@@ -1,40 +1,16 @@
-import 'source-map-support/register'
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { parseUserId } from "../auth/utils";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
+/**
+ * Get a user id from an API Gateway event
+ * @param event an event from API Gateway
+ *
+ * @returns a user id from a JWT token
+ */
+export function getUserId(event: APIGatewayProxyEvent): string {
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
 
-import { updateToDo } from '../../businessLogic/todos'
-import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
-// import { getUserId } from '../utils'
-
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
-    const authorization = event.headers.Authorization;
-    const split = authorization.split(' ');
-    const jwtToken = split[1];
-
-    const toDoItem = await updateToDo(updatedTodo, todoId, jwtToken);
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({
-            "item": toDoItem
-        }),
-    }
-  })
-
-handler
-  .use(httpErrorHandler())
-  .use(
-    cors({
-      credentials: true
-    })
-  )
+  return parseUserId(jwtToken)
+}
