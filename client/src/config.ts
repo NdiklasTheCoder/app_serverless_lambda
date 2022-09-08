b3211dd4fc92ad98bfaaa945d41a18f2075bc2a5
User <nicholas.v.ndiki@gmail>
2022-09-08 03:39:43 +0300

    Refactor - changed environment variables

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
-const apiId = 'qj0c402vw1';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+const apiId = '5ef7ezsf28';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-3v0c4-j1.us.auth0.com',
