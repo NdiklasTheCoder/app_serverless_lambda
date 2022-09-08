de145812b571c2c1cb4ff49f9ed29a4ca6a0e681
User <nicholas.v.ndiki@gmail>
2022-09-08 04:13:05 +0300

    Refactor - updated getToDo function logic

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,11 +1,16 @@
 import 'source-map-support/register'
 
-import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda';
-import {getAllToDo} from "../../businessLogic/ToDo";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { getAllToDo } from '../../businessLogic/todos'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // TODO: Get all TODO items for a current user
-    console.log("Processing Event ", event);
+// import { getUserId } from '../utils';
+
+// TODO: Get all TODO items for a current user
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // Write your code here
     const authorization = event.headers.Authorization;
     const split = authorization.split(' ');
     const jwtToken = split[1];
@@ -21,4 +26,13 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
             "items": toDos,
         }),
     }
-};
+
+    return undefined
+
+    handler.use(
+      cors({
+        credentials: true
+      })
+    )
+  }
+)
