ac95fdc57675dc0eef3d3858060e6834172400c9
User <nicholas.v.ndiki@gmail>
2022-09-08 04:26:06 +0300

    Chore - updated Auth0 params

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,11 @@
-const apiId = '5ef7ezsf28';
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 's3fwtp30m4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-3v0c4-j1.us.auth0.com',
-  clientId: 'QGAT8lZnZPwUsGS29KwSNGt297IOou65',
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-3v0c4-j1.us.auth0.com',            // Auth0 domain
+  clientId: 'QGAT8lZnZPwUsGS29KwSNGt297IOou65',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
-};
+}
