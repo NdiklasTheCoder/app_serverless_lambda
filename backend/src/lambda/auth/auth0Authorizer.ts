8bc1305ce4c530ca3881fd92a41a6f88ca335e89
User <nicholas.v.ndiki@gmail>
2022-09-08 04:07:18 +0300

    Chore - updated logic for auth in lambda

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,26 +1,25 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-import { verify} from 'jsonwebtoken'
+
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
-const logger = createLogger('auth');
+const logger = createLogger('auth')
 
-// Todo
-const jwksUrl = 'https://dev-3v0c4-j1.us.auth0.com/.well-known/jwks.json';
-
+// TODO: Provide a URL that can be used to download a certificate that can be used
+// to verify JWT token signature.
+// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
+const jwksUrl = 'https://dev-3v0c4-j1.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-
   logger.info('Authorizing a user', event.authorizationToken)
-
   try {
-
     const jwtToken = await verifyToken(event.authorizationToken)
-
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -37,7 +36,6 @@ export const handler = async (
       }
     }
   } catch (e) {
-
     logger.error('User not authorized', { error: e.message })
 
     return {
@@ -62,6 +60,8 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
     const token = getToken(authHeader)
     const res = await Axios.get(jwksUrl);
 
+    // TODO: Implement token verification
+    // You should implement it similarly to how it was implemented for the exercise for the lesson 5
     // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
     const pemData = res['data']['keys'][0]['x5c'][0]
     const cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
@@ -83,4 +83,3 @@ function getToken(authHeader: string): string {
 
   return token
 }
-
