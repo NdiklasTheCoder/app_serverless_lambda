cda79a5b4427d7419efa09477eaa0e82ac62da3d
User <nicholas.v.ndiki@gmail>
2022-09-14 22:38:17 +0300

    Refactor - add requestModel to resolve empty string issue

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'g5so9rkxjc'
+const apiId = 'zh3otsw0aj'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
